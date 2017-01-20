fd5014d145c5c9d627dc637abcf58c52d801259c
Sam <sam.rr.lee@gmail.com>
2017-01-20 18:35:56 +0000

    Made the print method recursive; Created 9x9 board test

--- board.py
+++ board.py
@@ -11,24 +11,31 @@ class Board:
         self.owner = None
 
     def __str__(self):
-        pieces = {None: ' ', 'O': 'O', 'X': 'X'}
+        piece = {None: ' ', 'O': 'O', 'X': 'X'}[self.owner]
         if self.rows == self.cols == 0:
-            return ("""
-             ---
-            | {} |
-             ---
-            """).format(pieces[self.owner])
-        elif self.rows != 3:
-            return "" # Not implemented yet
+            return piece
         else:
-            contents = [pieces[b.owner] for row in self.grid for b in row]
-            return ("""
-             {} | {} | {}
-            -----------
-             {} | {} | {}
-            -----------
-             {} | {} | {}
-            """).format(*contents)
+            result = ""
+            for i in range(len(self.grid)):
+                result_row = []
+                for j in range(len(self.grid[0])):
+                    substring = "\n".join([ " " + line + " " for line in self.grid[i][j].__str__().split('\n')])
+                    if i > 0:
+                        substring = "-" * (len(substring.split('\n')) + 2) + "\n" + substring
+                    if j > 0:
+                        new_substring = ""
+                        split_substring = substring.split("\n")
+                        for k in range(len(split_substring)):
+                            new_substring += "|" + split_substring[k] + "\n"
+                        substring = new_substring
+                    result_row.append(substring)
+                for k in range(len(result_row)):
+                    result_row[k] = result_row[k].split('\n')
+                for k in range(len(result_row[0])):
+                    for r in range(len(result_row)):
+                        result += result_row[r][k]
+                    result += '\n'
+            return result
 
     def perform_move(self, player, coords):
         if self.owner is None:
@@ -53,17 +60,24 @@ class Board:
 
 if __name__ == '__main__':
     grid = []
+
     for i in range(3):
-        row = []
+        subboard_row = []
         for j in range(3):
-            b = Board([])
-            row.append(b)
-        grid.append(row)
+            subsubboard = []
+            for k in range(3):
+                subsubboardrow = []
+                for l in range(3):
+                    subsubboardrow.append(Board([]))
+                subsubboard.append(subsubboardrow)
+            subboard_row.append(Board(subsubboard))
+        grid.append(subboard_row)
 
     main_board = Board(grid)
-    main_board.perform_move('O', [(0, 1), ()])
-    main_board.perform_move('X', [(2, 2), ()])
+    main_board.perform_move('O', [(0, 1), (0, 1), ()])
+    main_board.perform_move('X', [(0, 1), (2, 1), ()])
     print(main_board)
+    print("\n\n...\n\n")
     print(main_board.grid[0][1])
-
-
+    print("\n\n...\n\n")
+    print(main_board.grid[0][1].grid[0][1])
