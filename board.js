a2e6aaff0a2f0a19bdb737a83b03c49f4b97d50b
Sam <sam.rr.lee@gmail.com>
2021-01-16 15:21:24 +0000

    Finished javascript implementation

--- board.js
+++ board.js
@@ -27,24 +27,22 @@ function Board(cells) {
         this.layer += 1;
         inner_grid = inner_grid[0][0].grid;
     }
-    this.owner = null;
-
-    this.__repr__ = function() {
-        return "<Board at layer {}>".replace(/{}/g, this.layer);
-    };
+    this.owner = undefined;
 
     this.draw_board = function(active_coordinates) {
+        debugger;
         var piece;
-        if (this.owner == null)
+        //console.log("Owner: ", this.owner);
+        if (this.owner == undefined)
             piece = ' ';
         else
             piece = this.owner.toString();
 
-        var is_active = active_coordinates != null;
+        var is_active = active_coordinates != undefined;
         if (is_active)
             is_active = is_active && (active_coordinates.length > 0);
 
-        if (this.rows == this.cols == 0)
+        if (this.rows == 0 && this.cols == 0)
             return piece;
         else {
             var result = "";
@@ -52,45 +50,49 @@ function Board(cells) {
                 var result_rows = [];
                 for (var j = 0; j < this.cols; j++) {
                     var substr = "";
-                    if (is_active && arrayEq(active_coordinates[0], [j, i]))
+                    if (is_active && arrayEq(active_coordinates[0], [j, i])) {
                         substr = this.grid[i][j].draw_board(active_coordinates.slice(1));
-                    else
-                        substr = this.grid[i][j].draw_board(null);
+                    } else {
+                        substr = this.grid[i][j].draw_board(undefined);
+                    }
                     var lines_of_substr = substr.split('\n');
                     var str_length = lines_of_substr[0].length;
-                    if (this.grid[i][j].check_winner() == null)
+                    if (this.grid[i][j].check_winner() == undefined) {
                         substr = (lines_of_substr.map(ln => " " + ln + " ")).join("\n");
-                    else
-                        winner = this.grid[i][j].check_winner()
+                    } else {
+                        var winner = this.grid[i][j].check_winner()
                         substr = (lines_of_substr.map(_ => " ".repeat(str_length))).join("\n");
                         var substr_length = substr.length;
                         substr = substr.slice(0, Math.floor(substr_length/2)) + winner + substr.slice(Math.floor(substr_length/2) + 1, substr_length);
+                    }
                     lines_of_substr = substr.split('\n')
                     str_length = lines_of_substr[0].length
                     var blank_row;
                     if (str_length > 3) { //Check not single cell
-                        if (is_active && (active_coordinates.length == 1) && arrayEq(active_coordinates[0], [j, i]))
+                        if (is_active && (active_coordinates.length == 1) && arrayEq(active_coordinates[0], [j, i])) {
                             blank_row = "*" + " ".repeat(str_length - 2) + "*";
-                        else
+                        } else {
                             blank_row = " ".repeat(str_length);
-                        substr = "{0}\n{1}\n{0}".replace(/{0}/g, blank_row).replace(/{1}/g, substr);
+                        }
+                        substr = "{0}\n{1}\n{0}".replace(/\{0\}/g, blank_row).replace(/\{1\}/g, substr);
                     }
-                    if (i > 0) //Only after first row
+                    if (i > 0) { //Only after first row
                         substr = "-".repeat(str_length) + "\n" + substr;
+                    }
                     if (j > 0) { //Only after first column
                         lines_of_substr = substr.split('\n');
                         substr = ""; // Rebuild from here...
-                        for (var line in lines_of_substr)
-                            substr += "|{}\n".replace(/{}/g, line);
+                        for (var line of lines_of_substr)
+                            substr += "|\{\}\n".replace(/\{\}/g, line);
                     }
                     result_rows.push(substr);
                 }
                 result_rows = result_rows.map(x => x.split("\n"));
-                console.log(result_rows);
+                ////console.log(result_rows);
                 for (var k = 0; k < result_rows[0].length; k++) {
-                    for (var r in result_rows)
-                        result += result_rows[r][k];
-                    console.log(k, ': ', result);
+                    for (var r of result_rows)
+                        result += r[k];
+                    ////console.log(k, ': ', result);
                     result += '\n';
                 }
             }
@@ -99,11 +101,11 @@ function Board(cells) {
     };
         
 
-    this.__str__ = function() {
-        return this.draw_board(null)
+    this.str = function() {
+        return this.draw_board(undefined)
     };
 
-    this.__getitem__ = function(pos) {
+    this.getitem = function(pos) {
         return this.grid[pos[0]][pos[1]]
     };
 
@@ -139,7 +141,7 @@ function Board(cells) {
     }; 
 
     this.is_complete = function() {
-        if (this.owner != null)
+        if (this.owner != undefined)
             return true
         else if ((this.rows == 0) && (this.cols == 0))
             return false //No inner boards to check
@@ -149,7 +151,7 @@ function Board(cells) {
         for (var r = 0; r < this.rows; r++)
             for (var c = 0; c < this.cols; c++)
                 all_coords.push([r, c]);
-        for (var coord in all_coords)
+        for (var coord of all_coords)
             if (!self.grid[coord[0]][coord[1]].is_complete())
                 return false;
         return result;
@@ -157,7 +159,7 @@ function Board(cells) {
 
     this.is_valid_move = function(coords) {
         var board = this;
-        for (var c in coords)
+        for (var c of coords)
             board = board.grid[c[1]][c[0]];
         return !board.is_complete();
     };
@@ -173,16 +175,16 @@ function Board(cells) {
         the board is set to player (even if not at layer 0).
         */
         var coords = coords.slice()
-        if (this.owner != null)
+        if (this.owner != undefined)
             return false;
         if (!coords.length) { // should also check rows==0?
             this.owner = player;
             return true;
         } else {
-            console.log(coords);
+            //console.log(coords);
             var c = coords[0];
             var is_valid = this.grid[c[1]][c[0]].perform_move(player, coords.slice(1));
-            console.log(this);
+            //console.log(this);
             this.owner = this.check_winner(); //Only needs to be called if inner
                                               //board becomes completed
             return is_valid
@@ -196,22 +198,22 @@ function Board(cells) {
         player who owns a line of n cells, which can be vertical, horizontal,
         or diagonal through the centre cell.
         */
-        if (this.owner != null)
+        if (this.owner != undefined)
             return this.owner;
         var rows = this.get_rows();
         for (var i = 0; i < rows.length; i++) {
-            if (rows[i].filter(c => (c.owner == rows[i][0].owner) && (c.owner != null)).length == this.cols)
+            if (rows[i].filter(c => (c.owner == rows[i][0].owner) && (c.owner != undefined)).length == this.cols)
                 return r[i][0].owner;
         }
         var cols = this.get_cols();
-        console.log(cols);
+        //console.log(cols);
         for (var i = 0; i < cols.length; i++) {
-            if (cols[i].filter(c => (c.owner == cols[i][0].owner) && (c.owner != null)).length == this.rows)
+            if (cols[i].filter(c => (c.owner == cols[i][0].owner) && (c.owner != undefined)).length == this.rows)
                 return cols[i][0].owner;
         }
         var diags = this.get_diags();
         for (var i = 0; i < diags.length; i++) {
-            if (diags[i].filter(c => (c.owner == diags[i][0].owner) && (c.owner != null)).length == this.rows)
+            if (diags[i].filter(c => (c.owner == diags[i][0].owner) && (c.owner != undefined)).length == this.rows)
                 return diags[i][0].owner;
         }
     };
@@ -239,7 +241,7 @@ function parse_move(user_input) {
     try { // Catch the case input can't be converted to int
         move = user_input.map(x => +x);
     } catch(err) {
-        console.log(err);
+        //console.log(err);
         return;
     }
     // Check two numbers were entered (case of more than 2 already caught) and
